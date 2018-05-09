771011f8a02cd3b5443604d552441a0fe4a04bd5
TP\TP1709017 <yuechang@wistronits.com>
2018-05-09 12:08:32 +0800

    kick off - add the first textView

--- app/src/main/java/com/yue/pokemong7rngtool/MainActivity.java
+++ app/src/main/java/com/yue/pokemong7rngtool/MainActivity.java
@@ -1,14 +1,30 @@
 package com.yue.pokemong7rngtool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView btnEggRNG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEggRNG = (TextView) findViewById(R.id.btn_eggRNG);
+
+//        btnEggRNG.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent it = new Intent(MainActivity.this, ViewHistoricalDataActivity.class);
+//                startActivity(it);
+//            }
+//        });
+
     }
     //test
 }
