15e27656298bbafae824ea9148347a5599225ea3
TP\TP1709017 <yuechang@wistronits.com>
2018-12-26 17:06:31 +0800

    main rng method

--- app/src/main/java/com/yue/pokemong7rngtool/FindSeedActivity.java
+++ app/src/main/java/com/yue/pokemong7rngtool/FindSeedActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -11,26 +12,34 @@ import org.w3c.dom.Text;
 public class FindSeedActivity extends AppCompatActivity {
 
     private TextView btnStart;
-    private TextView txtResult1;
-    private TextView txtResult2;
-    private TextView txtResult3;
-    private TextView txtResult4;
+    private TextView txtResult;
+    private EditText txtMagikarpSeries;
+    String series = "0100011101000000011101101011101000001001100101101000110110101101111101010110100100101111010001001000110011101111010011101001010";
+    int[] result = new int[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_seed);
 
-        btnStart   = findViewById(R.id.btn_start_find_seed);
-        txtResult1 = findViewById(R.id.text_result1);
-        txtResult2 = findViewById(R.id.text_result2);
-        txtResult3 = findViewById(R.id.text_result3);
-        txtResult4 = findViewById(R.id.text_result4);
+        btnStart  = findViewById(R.id.btn_start_find_seed);
+        txtResult = findViewById(R.id.find_seed_result);
+        txtMagikarpSeries = findViewById(R.id.magikarp_series);
+
+        txtMagikarpSeries.setText(series);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO
+
+                result = MagikarpMethod.calc(series);
+
+                String statusString =
+                        Integer.toHexString(result[3])+", "+
+                                Integer.toHexString(result[2])+", "+
+                                Integer.toHexString(result[1])+", "+
+                                Integer.toHexString(result[0]);
+                txtResult.setText(statusString);
             }
         });
     }
---
+++ app/src/main/java/com/yue/pokemong7rngtool/TinyMTParameter.java
@@ -0,0 +1,14 @@
+package com.yue.pokemong7rngtool;
+
+public class TinyMTParameter {
+
+    int mat1;
+    int mat2;
+    int tmat;
+
+    public TinyMTParameter(int mat1, int mat2, int tmat) {
+        this.mat1 = mat1;
+        this.mat2 = mat2;
+        this.tmat = tmat;
+    }
+}
